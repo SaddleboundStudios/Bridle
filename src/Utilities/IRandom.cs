e16a28c385fbffc77aab493dd57f22d1b2402b3e
Walter Barrett <WalterBarrett@users.noreply.github.com>
2017-12-12 05:56:05 -0500

    Add missing license header.

--- src/Utilities/IRandom.cs
+++ src/Utilities/IRandom.cs
@@ -1,4 +1,16 @@
-﻿namespace Microsoft.Xna.Framework.Utilities
+﻿#region License
+/* Bridle
+ * Copyright 2017 Walter Barrett
+ *
+ * This project is released under the Microsoft Public
+ * License. This file is dual-licensed under the MS-PL
+ * and the 3-Clause BSD License.
+ *
+ * See LICENSE.MD for details.
+ */
+#endregion
+
+namespace Microsoft.Xna.Framework.Utilities
 {
     public interface IRandom
     {
